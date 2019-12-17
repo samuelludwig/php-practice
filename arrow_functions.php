9f4d07b4448e3d4df717da2f79df58c027ec77b2
samuelludwig <SLudwig.dev@gmail.com>
2019-12-17 03:43:31 +0000

    Add arrow function and webform practice

---
+++ arrow_functions.php
@@ -0,0 +1,5 @@
+<?php
+// Only works with php7.4 and up :(
+$add = fn($x, $y) => $x + $y;
+echo $add(2, 4);
+?>
---
+++ webform_handler/public/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+echo "Hello Web";
+?>
+
+<form action="send_info_to_db.php" method="post">
+Name: <input type="text" name="name"><br>
+<input type="submit">
+</form>
+
+</body>
+</html>
